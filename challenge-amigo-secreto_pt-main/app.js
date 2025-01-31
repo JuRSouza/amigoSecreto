d67722c763debe73dccb4960344f5a3e86328eac
Juliana Souza <121642110+JuRSouza@users.noreply.github.com>
2025-01-31 16:34:01 -0300

    Criada função AtualizarLista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -6,13 +6,27 @@ let nomeAmigo = [];
 
 function adicionarAmigo(){
     let entrada = document.querySelector('#amigo'); //Capturar o valor do campo de entrada
-    let lista = document.querySelector('#listaAmigos'); 
-
-//Validar a entrada
+//Validar a entrada- usando o trim para retirar os espaços
     if (entrada.value.trim() === '') {
         alert('Por favor, insira um nome.');  
         return; 
     }
+    //Atualizar o array de amigos, usando o método .push()
+    let nome = entrada.value.trim().toLowerCase();
+    nomeAmigo.push(nome);
+    entrada.value = ""; // limpando a entrada de nomes
+}
 
+function AtualizarLista(){
+    let lista = document.querySelector('#listaAmigos'); 
+    lista.innerHTML = ""; //Limpar a lista existente
+
+    //Percorrer o array: Use um loop for para percorrer o array amigos e criar elementos de lista (<li>) para cada nome.
+    for (let i = 0; i < nomeAmigo.length; i++){
+        let novoNome = document.createElement('li'); //adicione cada nome como um elemento <li> dentro de uma lista HTML.  
+        novoNome.textContent = nomeAmigo[i];
+        lista.appendChild(novoNome);// insere o nome na lista 
+    }
+            
 }
 
