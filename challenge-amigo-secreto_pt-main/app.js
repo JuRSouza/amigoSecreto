62dfba38fc67442c4d542834ee80e718780e87a1
Juliana Souza <121642110+JuRSouza@users.noreply.github.com>
2025-01-31 15:35:30 -0300

    Criada função adicionarAmigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomeAmigo = [];
+
+//Desenvolva uma função que permita ao usuário inserir um nome no campo de texto e adicioná-lo à lista de amigos criada anteriormente.
+
+function adicionarAmigo(){
+    let entrada = document.querySelector('#amigo'); //Capturar o valor do campo de entrada
+    let lista = document.querySelector('#listaAmigos'); 
+
+//Validar a entrada
+    if (entrada.value.trim() === '') {
+        alert('Por favor, insira um nome.');  
+        return; 
+    }
+
+}
+
